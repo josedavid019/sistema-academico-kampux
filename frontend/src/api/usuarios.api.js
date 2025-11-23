6fa65005bbd346bfd25e2127bb4a91e2bbfb0489
josedavid019 <joseangaritae@gmail.com>
2025-11-23 16:17:01 -0500

    Metodos completos en la api usuarios

--- frontend/src/api/usuarios.api.js
+++ frontend/src/api/usuarios.api.js
@@ -27,11 +27,59 @@ export const logoutUsuario = async () => {
 export const postEstudiante = async (estudiante) =>
   (await usuariosApi.post("estudiantes/", estudiante)).data;
 
+export const getEstudiantes = async () =>
+  (await usuariosApi.get("estudiantes/all/")).data;
+
+export const getEstudiante = async (id) =>
+  (await usuariosApi.get(`estudiantes/${id}/`)).data;
+
+export const putEstudiante = async (id, estudiante) =>
+  (await usuariosApi.put(`estudiantes/${id}/`, estudiante)).data;
+
+export const deleteEstudiante = async (id) =>
+  (await usuariosApi.delete(`estudiantes/${id}/`)).data;
+
 export const postDocente = async (docente) =>
   (await usuariosApi.post("docentes/", docente)).data;
+
+export const getDocentes = async () =>
+  (await usuariosApi.get("docentes/all/")).data;
+
+export const getDocente = async (id) =>
+  (await usuariosApi.get(`docentes/${id}/`)).data;
+
+export const putDocente = async (id, docente) =>
+  (await usuariosApi.put(`docentes/${id}/`, docente)).data;
+
+export const deleteDocente = async (id) =>
+  (await usuariosApi.delete(`docentes/${id}/`)).data;
 
 export const postCoordinador = async (coordinador) =>
   (await usuariosApi.post("coordinadores/", coordinador)).data;
 
+export const getCoordinadores = async () =>
+  (await usuariosApi.get("coordinadores/all/")).data;
+
+export const getCoordinador = async (id) =>
+  (await usuariosApi.get(`coordinadores/${id}/`)).data;
+
+export const putCoordinador = async (id, coordinador) =>
+  (await usuariosApi.put(`coordinadores/${id}/`, coordinador)).data;
+
+export const deleteCoordinador = async (id) =>
+  (await usuariosApi.delete(`coordinadores/${id}/`)).data;
+
 export const postAdministrador = async (administrador) =>
   (await usuariosApi.post("administradores/", administrador)).data;
+
+export const getAdministradores = async () =>
+  (await usuariosApi.get("administradores/all/")).data;
+
+export const getAdministrador = async (id) =>
+  (await usuariosApi.get(`administradores/${id}/`)).data;
+
+export const putAdministrador = async (id, administrador) =>
+  (await usuariosApi.put(`administradores/${id}/`, administrador)).data;
+
+export const deleteAdministrador = async (id) =>
+  (await usuariosApi.delete(`administradores/${id}/`)).data;
